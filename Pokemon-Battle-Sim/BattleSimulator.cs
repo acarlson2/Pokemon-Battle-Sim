84453a5a031780a605833a0c1724db41b04d0d67
Aaron Carlson <acarlson2@carthage.edu>
2024-07-08 14:37:19 -0500

    cleaned

--- Pokemon-Battle-Sim/BattleSimulator.cs
+++ Pokemon-Battle-Sim/BattleSimulator.cs
@@ -19,36 +19,39 @@ namespace Pokemon_Battle_Sim
 
 			var response = _client.GetStringAsync(pokeURL).Result;
 
-			var types = JObject.Parse(response).SelectToken("types");
-			var abil = JObject.Parse(response).SelectToken("abilities");
+			var data = JObject.Parse(response);
+
+			var types = data.SelectToken("types");
+			var abil = data.SelectToken("abilities");
+			var stats = data.SelectToken("stats");
 
             Console.WriteLine($"Here is the data for {name}:");
 
 			Console.WriteLine("Type: ");
 			for(int t = 0; t < types.Count(); t++)
 			{
-				Console.WriteLine("\t" + JObject.Parse(response).SelectToken($"types[{t}].type.name") + " ");
+				Console.WriteLine("\t" + data.SelectToken($"types[{t}].type.name") + " ");
 			}
 
 			Console.WriteLine("Abilities: ");
 			for(int a = 0; a < abil.Count(); a++)
 			{
-				if((bool)JObject.Parse(response).SelectToken($"abilities[{a}].is_hidden") == true)
+				if((bool)data.SelectToken($"abilities[{a}].is_hidden") == true)
 				{
-                    Console.WriteLine($"\t{a + 1}. " + JObject.Parse(response).SelectToken($"abilities[{a}].ability.name") +
+                    Console.WriteLine($"\t{a + 1}. " + data.SelectToken($"abilities[{a}].ability.name") +
 						" (Hidden Ability)");
                 }
 				else
 				{
-					Console.WriteLine($"\t{a + 1}. " + JObject.Parse(response).SelectToken($"abilities[{a}].ability.name"));
+					Console.WriteLine($"\t{a + 1}. " + data.SelectToken($"abilities[{a}].ability.name"));
                 }
 			}
 
 			Console.WriteLine("Stats:");
-			for(int s = 0; s < 6; s++)
+			for(int s = 0; s < stats.Count(); s++)
 			{
-				Console.WriteLine($"{JObject.Parse(response).SelectToken($"stats[{s}].stat.name")}: " +
-                    JObject.Parse(response).SelectToken($"stats[{s}].base_stat"));
+				Console.WriteLine($"{data.SelectToken($"stats[{s}].stat.name")}: " +
+                    data.SelectToken($"stats[{s}].base_stat"));
 			}
 		}
 	}
